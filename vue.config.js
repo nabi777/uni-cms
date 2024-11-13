313a2bd1dc31cf894016085faa5545d17cf41e87
melvin hue <melvinhue12345@gmail.com>
2024-11-13 22:10:34 +0800

    fixed version

--- vue.config.js
+++ vue.config.js
@@ -15,17 +15,30 @@
 //         }
 //     }
 // };
-const { defineConfig } = require('@vue/cli-service')
-const webpack = require('webpack');
+// const { defineConfig } = require('@vue/cli-service')
+// const webpack = require('webpack');
 
-module.exports = defineConfig({
+// module.exports = defineConfig({
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.DefinePlugin({
+//         // Vue CLI is in maintenance mode, and probably won't merge my PR to fix this in their tooling
+//         // https://github.com/vuejs/vue-cli/pull/7443
+//         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+//       })
+//     ],
+//   },
+// });
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = {
+  // Your configuration here
+  transpileDependencies: true,
   configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        // Vue CLI is in maintenance mode, and probably won't merge my PR to fix this in their tooling
-        // https://github.com/vuejs/vue-cli/pull/7443
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-      })
-    ],
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm-bundler.js', // ensure we're using the right build of Vue
+      },
+    },
   },
-});
+};
