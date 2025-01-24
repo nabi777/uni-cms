eb3b1475417bee52d2fd50e86a4768b7afe45930
Melvin <melvinhue12345@gmail.com>
2025-01-24 16:00:54 +0800

    updated port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001; //previously 3000
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
